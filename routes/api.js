c4772d1faf806d694a05fee6aeba44ffd6df397f
liuru123 <liuru@1457816105qq.com>
2016-08-04 15:46:32 +0800

    Bar2Post

--- routes/api.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by liuru on 16-8-2.
- */
-exports.index = function (req,res) {
-    res.json(200,{name:'张三',age:18});
-}
--- server.js
+++ server.js
@@ -4,16 +4,15 @@
 const express =require('express');
 const bodyParser =require('body-parser');
 const app = express();
-var api = require('./routes/api');
 var post = require('./routes/PostTransformBarcode');
 var barcode = require('./routes/BarcodeTransformPost');
  app.use(bodyParser.urlencoded({extend:true}));
 
 app.post('/PostTransformBarcode',function (req,res) {
-    res.send(new post().postTransformBarcode(req.body.posts));
+    res.send(new post().postTransformBarcode(req.body.code));
 });
 app.post('/BarcodeTransformPost',function (req,res) {
-    res.send(new barcode().BarcodeTransformPost(req.body.barcode));
+    res.send(new barcode().BarcodeTransformPost(req.body.code));
 });
 app.listen(3000,function () {
     console.log('Example app listening on port 3000!');
